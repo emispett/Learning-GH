5fba9dd30dbd4af2f68dfb13070031834e6dd87e
EV <emiliano.visconti@gmail.com>
2021-03-04 15:51:01 +0100

    Initial Commit

---
+++ M3-LearningGitHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M3-LearningGitHub
+//
+//  Created by Emiliano Visconti on 04/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M3-LearningGitHub/M3_LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  M3_LearningGitHubApp.swift
+//  M3-LearningGitHub
+//
+//  Created by Emiliano Visconti on 04/03/21.
+//
+
+import SwiftUI
+
+@main
+struct M3_LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
